fba297bcc9f2f5771c183267017e2f697bdf48f9
Luigi160397 <luigi.difraia.97@gmail.com>
2023-03-20 16:42:31 +0100

    2 fetch

--- assets/script/home.js
+++ assets/script/home.js
@@ -3,22 +3,24 @@ const url = "https://striveschool-api.herokuapp.com/api/deezer/search?q={query}"
 fetch(url)
   .then(res => res.json())
   .then(data => {
-    const row = document.querySelector("#card-playlist");
-    row.innerHTML = "";
+    const row1 = document.querySelector("#card-playlist");
+    row1.innerHTML = "";
+    const row2 = document.querySelector("#cards2");
+    row2.innerHTML = "";
     const canzoni = data.data;
     for (const canzone of canzoni) {
-      createCard(canzone.title, canzone.artist.picture_small);
+      createCard(canzone.title, canzone.artist.picture_medium, canzone.artist.name);
     }
   })
   .catch(error => console.log(error));
 
-const createCard = (title, img) => {
-  const row = document.querySelector("#card-playlist");
-  const col = document.createElement("div");
-  col.setAttribute("class", "col");
-  row.appendChild(col);
+const createCard = (title, img, artist) => {
+  const row1 = document.querySelector("#card-playlist");
+  const col1 = document.createElement("div");
+  col1.setAttribute("class", "col");
+  row1.appendChild(col1);
 
-  col.innerHTML = `<div style="background-color: #363636" class="card mb-3 border-0 text-light rounded-2">
+  col1.innerHTML = `<div style="background-color: #363636" class="card mb-3 border-0 text-light rounded-2">
     <div class="row g-0">
       <div class="col-md-2">
         <img
@@ -35,4 +37,21 @@ const createCard = (title, img) => {
       </div>
     </div>
   </div>`;
+
+  const row2 = document.querySelector("#cards2");
+  const col2 = document.createElement("div");
+  col2.setAttribute("class", "col");
+  row2.appendChild(col2);
+
+  col2.innerHTML = `<div class="col">
+  <div style="background-color: #171717" class="card border-0 text-light">
+    <div class="px-4 pt-4 pb-1 rounded-3">
+      <img src="${img}" class="card-img img-fluid" alt="${title}" />
+    </div>
+    <div class="card-body px-4">
+      <h5 class="card-title fs-5 text-truncate">${title}</h5>
+      <p class="card-text text-secondary">${artist}</p>
+    </div>
+  </div>
+</div>`;
 };
