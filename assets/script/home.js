c7e24151e2e9a936af0ddb1e003826321dcf1c3c
Luigi160397 <luigi.difraia.97@gmail.com>
2023-03-20 23:20:12 +0100

    card divise

--- assets/script/home.js
+++ assets/script/home.js
@@ -8,13 +8,20 @@ fetch(url)
     const row2 = document.querySelector("#cards2");
     row2.innerHTML = "";
     const canzoni = data.data;
-    for (const canzone of canzoni) {
+    shuffle(canzoni);
+    createCardPrincipale(canzoni[0].title, canzoni[0].artist.picture_medium, canzoni[0].artist.name);
+    for (let i = 0; i < 6; i++) {
+      const canzone = canzoni[i];
+      createCardPlaylist(canzone.title, canzone.artist.picture_medium);
+    }
+    for (let i = 6; i < 11; i++) {
+      const canzone = canzoni[i];
       createCard(canzone.title, canzone.artist.picture_medium, canzone.artist.name);
     }
   })
   .catch(error => console.log(error));
 
-const createCard = (title, img, artist) => {
+const createCardPlaylist = (title, img) => {
   const row1 = document.querySelector("#card-playlist");
   const col1 = document.createElement("div");
   col1.setAttribute("class", "col");
@@ -37,21 +44,60 @@ const createCard = (title, img, artist) => {
       </div>
     </div>
   </div>`;
+};
 
+const createCard = (title, img, artist) => {
   const row2 = document.querySelector("#cards2");
   const col2 = document.createElement("div");
   col2.setAttribute("class", "col");
   row2.appendChild(col2);
 
   col2.innerHTML = `<div class="col">
-  <div style="background-color: #171717" class="card border-0 text-light">
-    <div class="px-4 pt-4 pb-1 rounded-3">
-      <img src="${img}" class="card-img img-fluid" alt="${title}" />
+    <div style="background-color: #171717" class="card border-0 text-light">
+      <div class="px-4 pt-4 pb-1 rounded-3">
+        <img src="${img}" class="card-img img-fluid" alt="${title}" />
+      </div>
+      <div class="card-body px-4">
+        <h5 class="card-title fs-5 text-truncate">${title}</h5>
+        <p class="card-text text-secondary">${artist}</p>
+      </div>
     </div>
-    <div class="card-body px-4">
-      <h5 class="card-title fs-5 text-truncate">${title}</h5>
-      <p class="card-text text-secondary">${artist}</p>
+  </div>`;
+};
+
+const createCardPrincipale = (title, img, artist) => {
+  const cardPrincipale = document.querySelector("#card-principale");
+  cardPrincipale.innerHTML = `<div class="row g-0">
+    <div class="col-2">
+      <img src="${img}" class="img-fluid" alt="${title}" />
     </div>
-  </div>
-</div>`;
+    <div class="col-6">
+      <div class="card-body">
+        <h6 class="card-title fw-bold">ALBUM</h6>
+        <h1 class="card-title fw-bold">${title}</h1>
+        <p class="card-text fw-bold">${artist}</p>
+        <p class="card-text fw-bold">Ascolta il nuovo singolo di ${artist}!</p>
+        <div class="d-flex gap-2">
+          <button style="background-color: #1ed760" class="btn px-4 py-2 rounded-pill fw-bold">Play</button>
+          <button
+            style="background-color: #2125297c"
+            class="btn px-4 py-2 rounded-pill fw-bold text-light border border-1 border-light"
+          >
+            Salva
+          </button>
+          <button class="btn rounded-pill fw-bold text-light">
+            <i class="bi bi-three-dots text-secondary fs-3"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>`;
+};
+
+const shuffle = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 };
